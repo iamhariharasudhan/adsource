1860156ce366ca6cef321831ce7d3ab86b752e9e
iamhariharasudhan <151438260+iamhariharasudhan@users.noreply.github.com>
2025-02-21 13:24:01 +0530

    PaymentOption.java

--- PaymentOption.java
+++ PaymentOption.java
@@ -1,19 +1,76 @@
-package com.payment.phonepe;
+public class Product {
+    private int id;
+    private String title;
+    private String description;
+    private String image;
+    private double price;
 
-public class PaymentOption {
-    private String optionName;
-    private int imageResId; // Resource ID for the vector image
-
-    public PaymentOption(String optionName, int imageResId) {
-        this.optionName = optionName;
-        this.imageResId = imageResId;
+    // Getters and setters
+    public int getId() {
+        return id;
     }
 
-    public String getOptionName() {
-        return optionName;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public int getImageResId() {
-        return imageResId;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
+
+
+
+
+
+
+
+
+
+// package com.payment.phonepe;
+
+// public class PaymentOption {
+//     private String optionName;
+//     private int imageResId; // Resource ID for the vector image
+
+//     public PaymentOption(String optionName, int imageResId) {
+//         this.optionName = optionName;
+//         this.imageResId = imageResId;
+//     }
+
+//     public String getOptionName() {
+//         return optionName;
+//     }
+
+//     public int getImageResId() {
+//         return imageResId;
+//     }
+// }
